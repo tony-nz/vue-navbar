7e2126409f32a69def6257ce951eb0b265232728
Tony Myers <tony@myers.co.nz>
2023-04-07 23:05:08 +1200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="antialiased bg-gray-100 dark-mode:bg-gray-900 h-screen">
+    <NavBar />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+    NavBar,
   },
 });
 </script>
@@ -22,6 +27,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
