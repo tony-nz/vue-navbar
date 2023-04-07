31ba67127ff39839c70cec4ab4c3715057ed31c5
Tony Myers <tony@myers.co.nz>
2023-04-08 11:26:21 +1200

    Adding props for menu config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="antialiased bg-gray-100 dark-mode:bg-gray-900 h-screen">
-    <NavBar />
+    <NavBar :menu="menuConfig" />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
@@ -16,6 +16,61 @@ export default defineComponent({
   components: {
     HelloWorld,
     NavBar,
+  },
+  setup() {
+    const menuConfig = [
+      {
+        label: "Home",
+        link: "/",
+      },
+      {
+        label: "About",
+        link: "/about",
+      },
+      {
+        label: "Contact",
+        link: "/contact",
+      },
+      {
+        label: "Dropdown",
+        link: "/dropdown",
+        type: "list",
+        children: [
+          {
+            label: "Submenu 1",
+            link: "/submenu1",
+          },
+          {
+            label: "Submenu 2",
+            link: "/submenu2",
+          },
+        ],
+      },
+      {
+        label: "Grid Dropdown",
+        link: "/griddropdown",
+        type: "grid",
+        children: [
+          {
+            label: "Submenu 1",
+            link: "/submenu1",
+          },
+          {
+            label: "Submenu 2",
+            link: "/submenu2",
+          },
+          {
+            label: "Submenu 3",
+            link: "/submenu3",
+          },
+          {
+            label: "Submenu 4",
+            link: "/submenu4",
+          },
+        ],
+      },
+    ];
+    return { menuConfig };
   },
 });
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -36,7 +36,52 @@
         :class="{ flex: open, hidden: !open }"
         class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row"
       >
-        <a
+        <template v-for="item in menu" :key="item.label">
+          <a
+            v-if="!item.children"
+            :href="item.link"
+            class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+          >
+            {{ item.label }}
+          </a>
+          <div v-if="item.children" class="relative" x-data="{ open: false }">
+            <button
+              class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+              @click="open = !open"
+            >
+              <span>{{ item.label }}</span>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                :class="{ 'rotate-180': open, 'rotate-0': !open }"
+                class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div
+              x-show="open"
+              class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
+            >
+              <div
+                class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800"
+              >
+                <template v-for="child in item.children" :key="child.label">
+                  <a
+                    :href="child.link"
+                    class="block px-4 py-2 text-sm text-gray-700 transition-colors duration-200 transform rounded-md dark-mode:text-gray-200 hover:bg-gray-100 dark-mode:hover:bg-gray-700"
+                    >{{ child.label }}</a
+                  >
+                </template>
+              </div>
+            </div>
+          </div>
+        </template>
+        <!-- <a
           class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
           href="#"
           >Blog</a
@@ -45,16 +90,6 @@
           class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
           href="#"
           >Portfolio</a
-        >
-        <a
-          class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-          href="#"
-          >About</a
-        >
-        <a
-          class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-          href="#"
-          >Contact</a
         >
         <div class="relative" x-data="{ open: false }">
           <button
@@ -218,7 +253,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </nav>
     </div>
   </div>
@@ -229,5 +264,14 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "NarBar",
+  props: {
+    menu: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup() {
+    return {};
+  },
 });
 </script>
