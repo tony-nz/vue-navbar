31ea970d155ffbd8dad34da59eefe65968cd37b0
Tony Myers <tony@myers.co.nz>
2023-04-09 11:55:16 +1200

    Removing bg-transparent

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-on:mouseover="isOpen = true" v-on:mouseleave="isOpen = false">
     <button
-      class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-md dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+      class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left rounded-md dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
       @click="isOpen = !isOpen"
     >
       <span>{{ item.label }}</span>
@@ -58,7 +58,7 @@
             >
               <button
                 @click="navigate"
-                class="flex flex row w-full text-left items-start rounded-lg bg-transparent p-2 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                class="flex flex row w-full text-left items-start rounded-lg p-2 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                 :class="{
                   'text-gray-900 bg-gray-200': isActive,
                   'text-gray-900 bg-gray-200': isExactActive,
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -43,7 +43,7 @@
           >
             <button
               @click="navigate"
-              class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-md dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+              class="px-4 py-2 mt-2 text-sm font-semibold rounded-md dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
               :class="{
                 'text-gray-900 bg-gray-200': isActive,
                 'text-gray-900 bg-gray-200': isExactActive,
